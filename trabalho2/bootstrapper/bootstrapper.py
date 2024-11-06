dfeaaaa2da75183a8c93eef9afe45e09bfb1ee89
henrique vaz <henriquevaz7@hotmail.com>
2024-11-06 11:20:38 +0000

    bootstrapper

---
+++ trabalho2/bootstrapper/bootstrapper.py
@@ -0,0 +1,61 @@
+import sys
+import json
+import socket
+import threading
+
+def readJsonFile(filePath):
+    print("Reading file: " + filePath)
+    with open(filePath, 'r') as file:
+        return json.load(file)
+
+
+def handle_client(client_socket, addr, vizinhancas):
+    client_ip = addr[0]
+    
+    if client_ip in vizinhancas:
+        neighbors = vizinhancas[client_ip]
+        response = ', '.join(neighbors)
+        client_socket.send(response.encode())
+    else:
+        response = "ERROR"
+        client_socket.send(response.encode())
+    
+    client_socket.close()
+
+
+def serve_vizinhos(vizinhacas):
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(('0.0.0.0', 12222))
+    server_socket.listen(10)
+
+    print("Bootstrapper server is listening on port 12222...")
+
+    while True:
+        client_socket, addr = server_socket.accept()
+        print(f"Connection from {addr} has been established for retrieving neighbour info.")
+        client_handler = threading.Thread(target=handle_client, args=(client_socket, addr,vizinhacas))
+        client_handler.start()
+
+
+
+
+def main():
+    args = sys.argv[1:]
+
+    if "--file" in args:
+        file_index = args.index("--file") + 1
+
+        if (file_index < len(args) and not args[file_index].startswith("--")):
+            vizinhancas = readJsonFile(args[file_index])
+        else:
+            print("No file path provided after --file.")
+    else:
+        print("No --file flag provided, bootstrapper needs to read a valid json file!")
+
+    requestsThread = threading.Thread(target=serve_vizinhos, args=(vizinhancas,))
+    requestsThread.start()
+    requestsThread.join()
+
+
+if __name__ == "__main__":
+    main()
